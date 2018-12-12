698de539b78dc87ce7cdab99c9ea1a6e0b9d0075
zzc <370531378@qq.com>
2018-12-12 21:46:15 +0800

    加载动画效果

--- draw-pikachu/main.js
+++ draw-pikachu/main.js
@@ -1,19 +1,22 @@
 !function(){
+    let speed=50;
+    let timeid
     function writeCode(prefix,code,callback){
         let container=document.getElementById('code');
         let styleTag=document.querySelector('#styleTag');
         let n=0;
-        let timeid=setInterval(()=>{
+
+        timeid=setTimeout(function run(){
             n+=1;
             container.innerHTML=code.substring(0,n);
             styleTag.innerHTML=code.substring(0,n)
             container.scrollTop=container.scrollHeight;
-            if(n>=code.length){
-                console.log('停止了')
-                window.clearInterval(timeid)
+            console.log(speed)
+            if(n<=code.length){
+                timeid=setTimeout(run,speed)
                 callback&&callback()
             }
-        },50)
+        },speed)
     }
     let code=`
     .preview{
@@ -155,7 +158,39 @@
 
     $('input').on('click',function(e){
         let $button=$(e.currentTarget)
-        console.log($button.val())
+        switch ($button.val()) {
+            case '慢速':
+                speed = 100;
+                break;
+            case '中速':
+                speed = 60;
+                break;
+            case '快速':
+                speed = 20;
+                break;
+            case '加速':
+                if(speed>=5){
+                    speed -= 5;break;
+                }
+                else{
+                    alert('已到最快速了')
+                    break;
+                }
+
+            case '减速':
+                speed += 20;
+                break;
+            case '中断':
+                console.log(code);
+                $('#code').append(code);
+                document.getElementById('code').scrollTop=document.getElementById('code').scrollHeight
+                $('#styleTag').text(code);
+                clearTimeout(timeid);
+                console.log('中断了')
+                break;
+        }
+        $(this).addClass('active').siblings().removeClass('active')
+          /*  console.log(speed);*/
 
     })
 }.call()
